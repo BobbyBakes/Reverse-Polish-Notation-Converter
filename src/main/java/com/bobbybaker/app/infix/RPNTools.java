5f92899cb68f2e6e21482c0afda9b81cc12aab17
bobbybakes <bobby.a.baker@gmail.com>
2016-10-26 18:44:13 -0400

    Abstracted logic into private methods

--- src/main/java/com/bobbybaker/app/infix/RPNTools.java
+++ src/main/java/com/bobbybaker/app/infix/RPNTools.java
@@ -10,14 +10,11 @@ public class RPNTools {
     private static final String LEFT_PARENTHESIS = "(";
     private static final String RIGHT_PARENTHESIS = ")";
 
-    public static boolean isOperator(String operator) {
-        return OPERATORS.contains(operator);
-    }
-
     public static String convert(String RPN) {
-        int indexOfLastCharacter = RPN.length() - 1;
         String[] RPNArray = RPN.split("");
+        int indexOfLastCharacter = RPNArray.length - 1;
         Stack<String> equationStack = new Stack<String>();
+
         for (int i = 0; i < RPNArray.length; i++) {
             String operandOrOperator = String.valueOf(RPN.charAt(i));
             if (isOperator(operandOrOperator)) {
@@ -25,25 +22,37 @@ public class RPNTools {
             } else {
                 equationStack.add(operandOrOperator);
             }
-
         }
-        return equationStack.pop();
+        return equationStack.peek();
+    }
+
+    static boolean isOperator(String operator) {
+        return OPERATORS.contains(operator);
     }
 
     private static boolean shouldAddParenthesis(int i, int indexOfLastCharacter) {
         return i != indexOfLastCharacter;
     }
 
-    private static void determineOperatorPlacementAndAddToStack(Stack<String> equationStack, String t, boolean addParenthesis) {
+    private static void determineOperatorPlacementAndAddToStack(Stack<String> equationStack, String operator, boolean addParenthesis) {
         StringBuilder newNotation = new StringBuilder();
         int insertOperatorAtPosition = equationStack.peek().length();
-        newNotation.insert(newNotation.length(), equationStack.pop());
-        newNotation.insert(0, equationStack.pop());
-        newNotation.insert(newNotation.length() - insertOperatorAtPosition, t);
+
+        placeOperator(equationStack, operator, newNotation, insertOperatorAtPosition);
         if (addParenthesis) {
-            newNotation.insert(0, LEFT_PARENTHESIS);
-            newNotation.insert(newNotation.length(), RIGHT_PARENTHESIS);
+            addParenthesis(newNotation);
         }
         equationStack.add(newNotation.toString());
+    }
+
+    private static void addParenthesis(StringBuilder newNotation) {
+        newNotation.insert(0, LEFT_PARENTHESIS);
+        newNotation.insert(newNotation.length(), RIGHT_PARENTHESIS);
+    }
+
+    private static void placeOperator(Stack<String> equationStack, String operator, StringBuilder newNotation, int insertOperatorAtPosition) {
+        newNotation.insert(newNotation.length(), equationStack.pop());
+        newNotation.insert(0, equationStack.pop());
+        newNotation.insert(newNotation.length() - insertOperatorAtPosition, operator);
     }
 }
